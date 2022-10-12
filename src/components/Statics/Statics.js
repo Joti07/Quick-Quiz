9cf0e6b3644c62d04df40bf4a389f27755f596c7
Tanjila Sultana Joti <44638932+Joti07@users.noreply.github.com>
2022-10-12 19:42:52 +0600

    fix statics

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import './Statics.css';
-import 'react-toastify/dist/ReactToastify.css';
 import { useLoaderData } from 'react-router-dom';
-import { Bar, BarChart, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, CartesianGrid, Legend, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import { Tooltip } from 'react-bootstrap';
 const Statics = () => {
 
     const QuizData = useLoaderData();
+    // console.log(QuizData)
     return (
-        <div>
-            <BarChart width={830} height={250} data={QuizData.data}>
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="name" />
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Bar dataKey="total" fill="#8884d8" />
-            </BarChart>
+        <div className='lg:px-96 md:mt-5 px-0 mt-5'>
+
+            <ResponsiveContainer width='92%' height={400}>
+                <BarChart data={QuizData.data}>
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="total" fill="#8884d8" />
+                </BarChart>
+            </ResponsiveContainer>
 
         </div>
     );
