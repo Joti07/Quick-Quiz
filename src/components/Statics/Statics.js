76d91ea5fad20af49a89a77fd6ce81ee3a148427
Joti <tanjilajoti@gmail.com>
2022-10-12 16:48:18 +0600

    add statics

--- src/components/Statics/Statics.js
+++ src/components/Statics/Statics.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Statics.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useLoaderData } from 'react-router-dom';
+import { Bar, BarChart, CartesianAxis, CartesianGrid, Legend, XAxis, YAxis } from 'recharts';
+import { Tooltip } from 'react-bootstrap';
 const Statics = () => {
     const notify = () => toast.success('🦄 Wow so easy!', {
         position: "top-center",
@@ -13,11 +16,19 @@ const Statics = () => {
         progress: undefined,
         theme: "colored",
     });
-
+    const QuizData = useLoaderData();
     return (
         <div>
             <ToastContainer />
             <button onClick={notify}>Notify!</button>
+            <BarChart width={830} height={250} data={QuizData.data}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Bar dataKey="total" fill="#8884d8" />
+            </BarChart>
 
         </div>
     );
